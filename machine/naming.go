81d87936ff4c8c7a3b4992ac793dd552e973a5e6
Katsuma Ito <katsumai@gmail.com>
2018-05-11 14:26:21 +0900

    fix crash when MAC Address is not valid one

--- machine/naming.go
+++ machine/naming.go
@@ -5,5 +5,8 @@ import "strings"
 // ShortName : Get short name from macAddr
 func ShortName(macAddr string) string {
 	segs := strings.Split(macAddr, ":")
+	if len(segs) < 4 {
+		return ""
+	}
 	return strings.Join(segs[len(segs)-3:len(segs)], "")
 }
