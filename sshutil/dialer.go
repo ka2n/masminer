659c665df36e798c4d5997b1b8a020ebffbc64ef
Katsuma Ito <katsumai@gmail.com>
2018-10-18 02:04:00 +0900

    Extract ssh.Dial code and set ReadDeadline on net.Conn

--- sshutil/dialer.go
+++ sshutil/dialer.go
@@ -1,7 +1,7 @@
 package sshutil
 
 import (
-	"fmt"
+	"net"
 	"time"
 
 	"golang.org/x/crypto/ssh"
@@ -24,19 +24,22 @@ func (d *TimeoutDialer) DialTimeout(network, addr string, config *ssh.ClientConf
 		config.Timeout = timeout
 	}
 
-	done := make(chan struct{}, 1)
-	go func() {
-		defer close(done)
-		client, err = ssh.Dial(network, addr, config)
-	}()
-
-	select {
-	case <-done:
-		return client, err
-	case <-time.After(timeout):
-		if client != nil {
-			client.Close()
-		}
-		return nil, fmt.Errorf("timed out dialing %s:%s", network, addr)
+	conn, err := net.DialTimeout(network, addr, config.Timeout)
+	if err != nil {
+		return nil, err
 	}
+
+	if config.Timeout > 0 {
+		conn.SetReadDeadline(time.Now().Add(config.Timeout))
+	}
+
+	c, chans, reqs, err := ssh.NewClientConn(conn, addr, config)
+	if err != nil {
+		return nil, err
+	}
+
+	if config.Timeout > 0 {
+		conn.SetReadDeadline(time.Time{})
+	}
+	return ssh.NewClient(c, chans, reqs), nil
 }
